57ebb74dd7095489277b009d3373d319cb2e6ea8
dongxiaoping1015 <xiaoping_1015@163.com>
2018-10-28 14:55:40 +0800

    视图解析器

--- src/dong/handlers/MyController.java
+++ src/dong/handlers/MyController.java
@@ -8,7 +8,7 @@ public class MyController implements Controller {
     public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("message", "Hello SpringMVC world");
-        modelAndView.setViewName("/WEB-INF/jsp/welcome.jsp");
+        modelAndView.setViewName("welcome");
         return modelAndView;
     }
 }
