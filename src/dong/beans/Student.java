b7213239899ffb975ddfc794bf9823d78de558b6
dongxiaoping1015 <xiaoping_1015@163.com>
2018-10-31 06:16:35 +0800

    Ajax获取自定义对象

---
+++ src/dong/beans/Student.java
@@ -0,0 +1,27 @@
+package dong.beans;
+
+public class Student {
+    String name;
+    Integer age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/dong/handlers/MyController.java
+++ src/dong/handlers/MyController.java
@@ -1,14 +1,25 @@
 package dong.handlers;
 
+import dong.beans.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class MyController implements Controller {
-    @Override
-    public ModelAndView handleRequest(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("message", "Hello SpringMVC world");
-        modelAndView.setViewName("welcome");
-        return modelAndView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@org.springframework.stereotype.Controller
+public class MyController {
+    @RequestMapping({"/my.do", "/hello.do"})
+    @ResponseBody
+    public Object handleRequest(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("张三", 29));
+        students.add(new Student("李四", 21));
+        students.add(new Student("王五", 20));
+        return students;
     }
 }
